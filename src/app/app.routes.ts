ff947558bb963e39b9da1c772f72534b30a9c5b9
marjaneAbk <marjane.aberkan@lmdsio.fr>
2023-11-09 16:42:30 +0100

    1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import {ComposantRouteurComponent} from "./composant-routeur/composant-routeur.component";
 
-const routes: Routes = [
+export const routes: Routes = [
   { path:'login', component: ComposantRouteurComponent}
 ]
--- src/app/composant-selecteur/composant-selecteur.component.ts
+++ src/app/composant-selecteur/composant-selecteur.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {CommonModule} from "@angular/common";
+
 
 @Component({
   selector: 'app-composant-selecteur',
@@ -8,6 +10,10 @@ import { CommonModule } from '@angular/common';
   templateUrl: './composant-selecteur.component.html',
   styleUrl: './composant-selecteur.component.css'
 })
-export class ComposantSelecteurComponent {
+export class ComposantSelecteurComponent implements OnInit{
+  constructor(public router: Router){}
 
+  ngOnInit() :void{
+  }
 }
+
